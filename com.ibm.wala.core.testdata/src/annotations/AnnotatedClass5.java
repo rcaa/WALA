addb0d110685dda9d5c3f73a950d250d5d948a80
Martin Hecker <martin.hecker@kit.edu>
2016-11-27 15:38:34 +0100

    Some testdata for type annotations tests

---
+++ com.ibm.wala.core.testdata/src/annotations/AnnotatedClass5.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Joana IFC project,
+ * Programming Paradigms Group,
+ * Karlsruhe Institute of Technology (KIT).
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    martin.hecker@kit.edu, KIT - 
+ *******************************************************************************/
+package annotations;
+
+@TypeAnnotationTypeUse
+public class AnnotatedClass5 {
+  
+  @TypeAnnotationTypeUse Integer foo(@TypeAnnotationTypeUse int a, @TypeAnnotationTypeUse Object b) {
+    
+    @TypeAnnotationTypeUse
+    int x = 3;
+    
+    @TypeAnnotationTypeUse
+    Object y = new Object();
+    
+    if (y instanceof @TypeAnnotationTypeUse String) {
+      x = 7;
+    }
+    
+    try {
+      throw new NullPointerException();
+    } catch (@TypeAnnotationTypeUse RuntimeException e) {
+    }
+    
+    return x;
+  }
+}
